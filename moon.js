4701fd3dcd41f1862265c393a0ec0f690d878efe
Necis Zhang <necis.zhang@tbwa.com>
2018-03-23 19:40:22 +0800

    update

--- moon.js
+++ moon.js
@@ -2,70 +2,105 @@
 
 	function Moon(opts) {
 		//el is a node
+		// var el = document.querySelector(opts.el),
+		// 	model = opts.model,
+		// 	start = opts.start || "{{",
+		// 	end = opts.end || "}}";
+		// log(model);
+
+		// //one way data binding
+		// for (var key in model) {
+		// 	if (model.hasOwnProperty(key)) {
+		// 		var obj = {};
+		// 		Object.defineProperty(obj, key, {
+		// 			get: function() {
+		// 				return model[key]
+		// 			},
+		// 			set: function(val) {
+		// 				model[key] = val;
+		// 				update(start, end, key, model[key], el);
+		// 			}
+		// 		});
+		// 		update(start, end, key, model[key], el);
+		// 	}
+		// }
+
+		// //two way data binding
+		// console.log(el.childNodes);
+		// console.log(el.children);
+		// // for (var i = 0; i < el.childNodes.length; i++) {
+		// // 	if (el.childNodes[i].hasAttribute('m-model')) {
+		// // 		var modelval = el.childNodes[i].value;
+		// // 		update(start, end);
+		// // 	}
+		// // }
+
+		// for (var i = 0; i < el.children.length; i++) {
+		// 	var child=el.children[i];
+		// 	if(child.hasAttribute('m-model')){
+		// 		console.log(child);
+		// 		var modelVal = child.value;
+		// 		var modelName=child.getAttribute('m-model');
+		// 		opts.model[modelName]=modelVal;
+
+		// 		console.log(modelName);
+		// 		console.log(modelVal);
+
+		// 		// update(start,end,modelName,modelVal,el);
+		// 		child.addEventListener(['keyup','blur'],function(e){
+		// 			var name =e.target.getAttribute('m-model');
+		// 			console.log(name);
+		// 			if(name){
+		// 				if(e.target.value!=model[name]){
+		// 					opts.model[name]=e.target.value;
+		// 				}
+		// 			}
+		// 		})
+		// 	}
+		// }
+
 		var el = document.querySelector(opts.el),
 			model = opts.model,
 			start = opts.start || "{{",
 			end = opts.end || "}}";
-		log(model);
 
-		//one way data binding
-		for (var key in model) {
-			if (model.hasOwnProperty(key)) {
-				var obj = {};
-				Object.defineProperty(obj, key, {
-					get: function() {
-						return model[key]
-					},
-					set: function(val) {
-						model[key] = val;
-						update(start, end, key, model[key], el);
-					}
-				});
-				update(start, end, key, model[key], el);
-			}
-		}
 
-		//two way data binding
-		console.log(el.childNodes);
-		console.log(el.children);
-		// for (var i = 0; i < el.childNodes.length; i++) {
-		// 	if (el.childNodes[i].hasAttribute('m-model')) {
-		// 		var modelval = el.childNodes[i].value;
-		// 		update(start, end);
-		// 	}
-		// }
+		var update = function(start, end, key, replace, el) {
+			var pattern = start + key + end;
+			el.innerHTML = el.innerHTML.replace(new RegExp(pattern, 'g'), replace);
+		};
 
-		for (var i = 0; i < el.children.length; i++) {
-			var child=el.children[i];
-			if(child.hasAttribute('m-model')){
-				console.log(child);
-				var modelVal = child.value;
-				var modelName=child.getAttribute('m-model');
-				model[modelName]=modelVal;
-
-				console.log(modelName);
-				console.log(modelVal);
-
-				// update(start,end,modelName,modelVal,el);
-				child.addEventListener(['keyup','blur'],function(e){
-					var name =e.target.getAttribute('m-model');
-					console.log(name);
-					if(name){
-						if(e.target.value!=model[name]){
-							this.model[name]=e.target.value;
+		function getProxyModel() {
+			// body...
+			var obj={};
+			fo(var key in model){
+				if(model.hasOwnProperty(key)){
+					Object.defineProperty(obj,key,{
+						get:function () {
+							// body...
+							return model[key];
+						},
+						set:function(val){
+							model[key]=val;
+							update(start,end,model[key],el);
 						}
-					}
-				})
+					});
+					update(start,end,key,model[key],el);
+				}
 			}
+			return obj;
 		}
 
+		model=getProxyModel();
 
+		for(var i=0;i<el.children.length;i++){
+			var child=el.children[i];
+			if(child.hasOwnProperty('m-model')){
+				var modelVal=child.value;
+				var modelName=child.get
+			}
+		}
 	}
-
-	var update = function(start, end, key, replace, el) {
-		var pattern = start + key + end;
-		el.innerHTML = el.innerHTML.replace(new RegExp(pattern, 'g'), replace);
-	};
 
 	window.Moon = Moon;
 
@@ -74,7 +109,7 @@
 		model: {
 			'a': 1,
 			'b': 3,
-			'c':333
+			'c': 333
 		}
 
 	}
